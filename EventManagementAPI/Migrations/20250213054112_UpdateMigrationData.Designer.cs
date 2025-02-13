1acb06c994b885218c13cd5e54f7b7d10dec324d
Mostafa Atef <dev.mostafa.atef@gmail.com>
2025-02-13 07:43:36 +0200

    update migration scripts

--- EventManagementAPI/Migrations/20250213054112_UpdateMigrationData.Designer.cs
+++ EventManagementAPI/Migrations/20250213054112_UpdateMigrationData.Designer.cs
@@ -4,6 +4,7 @@ using EventManagementApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventManagementAPI.Migrations
 {
     [DbContext(typeof(EventContext))]
-    partial class EventContextModelSnapshot : ModelSnapshot
+    [Migration("20250213054112_UpdateMigrationData")]
+    partial class UpdateMigrationData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
